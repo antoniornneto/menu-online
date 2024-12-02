8339b18d1af8d854d297c92189c55c5affd55426
Antonio Nascimento <antonionascneto@gmail.com>
2024-12-02 09:19:14 -0300

    fix

--- src/app/api/edit-item/[id]/route.ts
+++ src/app/api/edit-item/[id]/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function PUT(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   const { id } = await params;
   const { name, description, price, category, quantity } = await req.json();
