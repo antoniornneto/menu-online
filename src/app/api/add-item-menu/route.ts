bbd6992da2f360979b1c9497ae3caac48f3e9bc7
Antonio Nascimento <antonionascneto@gmail.com>
2024-12-02 09:08:31 -0300

    fix

--- src/app/api/add-item-menu/route.ts
+++ src/app/api/add-item-menu/route.ts
@@ -1,11 +1,12 @@
-import { ItemProp } from "@/components/form/ModalAddItem";
 import { prisma } from "@/lib/prisma";
+import { ItemProp } from "@/lib/type";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   const { category, name, description, price, quantity }: ItemProp =
     await req.json();
-  const db = await prisma.menu.create({
+
+  await prisma.menu.create({
     data: {
       category,
       name,
